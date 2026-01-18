d01b18f1d01dfcedca1be2e3d2b43442745c8b56
sikiru <sikirurazak1@gmail.com>
2026-01-18 02:13:12 +0100

    [REFACTOR] unused constants

--- src/lib/constants/votes.ts
+++
@@ -1,9 +0,0 @@
-export const AGREE_VOTES = new Map([
-  [true, "AGREE"],
-  [false, "NOT_AGREE"],
-]);
-
-export const DISAGREE_VOTES = new Map([
-  [true, "DISAGREE"],
-  [false, "NOT_DISAGREE"],
-]);
